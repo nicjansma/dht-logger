d566315d8db4236bd2527971561838316cced442
Nic Jansma <nic@nicj.net>
2016-02-26 23:28:45 -0500

    Log all parts of the API Gateway payload

--- lambda.js
+++ lambda.js
@@ -6,46 +6,62 @@
 //
 // Based on: https://snowulf.com/2015/08/05/tutorial-aws-api-gateway-to-lambda-to-dynamodb/
 //
-console.log("Loading event");
 
+//
+// Imports
+//
 var doc = require("dynamodb-doc");
+
+//
+// Constants
+//
+var TABLE_NAME = "YOURTABLENAME";
+
 var dynamodb = new doc.DynamoDB();
 
 exports.handler = function(event, context) {
     console.log("Request received:\n", JSON.stringify(event));
     console.log("Context received:\n", JSON.stringify(context));
 
-    var tableName = "YOURTABLENAME";
-
     var datetime = new Date().getTime();
 
     var item = {
         "device": event.device, 
         "time": datetime,
+        "coreid": event.coreid,
+        "event": event.event
     };
 
-    for (var key in event) {
-        // skip some Particle Webhook properties
-        if (key === "data" || key === "published_at") {
-            continue;
-        }
+    // parse data payload
+    var data;
+    try {
+        data = JSON.parse(event.data);
+    } catch (e) {
+        return context.fail("Failed to parse data JSON");
+    }
 
-        if (event.hasOwnProperty(key)) {
-            item[key] = event[key];
+    // copy data properties over to DynamoDB item
+    for (var key in data) {
+        if (data.hasOwnProperty(key) &&
+            typeof data[key] !== "undefined" &&
+            data[key] !== null &&
+            data[key] !== "") {
+            item[key] = data[key];
         }
     }
 
     console.log("Item:\n", item);
 
+    // Put into DynamoDB!
     dynamodb.putItem({
-        "TableName": tableName,
+        "TableName": TABLE_NAME,
         "Item": item
     }, function(err, data) {
         if (err) {
             context.fail("ERROR: Dynamo failed: " + err);
         } else {
             console.log("Dynamo Success: " + JSON.stringify(data, null, "  "));
-            context.succeed("SUCCESS");
+            context.succeed();
         }
     });
 }
